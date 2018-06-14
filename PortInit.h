1461b88503049bb2fbe031788c8d218f7656e53d
James Vlasblom <jamesvlasblom@gmail.com>
2018-06-14 11:56:49 -0700

    Configured PortInit with appropriate port pins.

---
+++ PortInit.h
@@ -0,0 +1,282 @@
+
+/* 
+ * File:        PortInit.h
+ * Author:      James Vlasblom
+ * Date:        June 14, 2018
+ * 
+ * Comments:    This file contains the function 'PortInit()', 
+ *              which contains all the code for all the PORT initialization
+ *              including input / output, open drain, and initial value. PortB
+ *              also has Analog / Digital designation.
+ * 
+ *              This file also contains the necessary RPx pin re-mapping for
+ *              the required modules.
+ * 
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef PORTINIT_50_Shot_H
+#define	PORTINIT_50_Shot_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+
+
+void PortInit(void);
+
+void PortInit(void) {
+
+    //************************
+    //************************
+    //*         ADC          *
+    //************************
+    //************************
+
+    // *** Setting Inputs as Analog or Digital (PORTB only) ***
+
+    //AD1PCFG = 0xFF;     // set entire PORT digital
+
+    _PCFG0 = 1; // 0 = analog, 1 = digital
+    _PCFG1 = 0; // 0 = analog, 1 = digital
+    _PCFG2 = 1; // 0 = analog, 1 = digital
+    _PCFG3 = 1; // 0 = analog, 1 = digital
+    _PCFG4 = 1; // 0 = analog, 1 = digital
+    _PCFG5 = 0; // 0 = analog, 1 = digital
+    _PCFG6 = 1; // 0 = analog, 1 = digital
+    _PCFG7 = 1; // 0 = analog, 1 = digital
+    _PCFG8 = 1; // 0 = analog, 1 = digital
+    _PCFG9 = 1; // 0 = analog, 1 = digital
+    _PCFG10 = 1; // 0 = analog, 1 = digital
+    _PCFG11 = 1; // 0 = analog, 1 = digital
+    _PCFG12 = 1; // 0 = analog, 1 = digital
+    // no _PCFG13.
+    // no _PCFG13.
+    _PCFG15 = 1; // 0 = analog, 1 = digital
+
+
+    //*** All PORTS are inputs at POR ***
+
+    //************************
+    //************************
+    //* PortA initialization *
+    //************************
+    //************************
+
+    //*** INPUT / OUTPUT Configuration***
+
+    //TRISA = 0x00;      // Set entire PORT
+
+    _TRISA0 = 0; // 0 = output, 1 = input 
+    _TRISA1 = 0; // 0 = output, 1 = input
+    _TRISA2 = 0; // 0 = output, 1 = input
+    _TRISA3 = 0; // 0 = output, 1 = input 
+    _TRISA4 = 0; // 0 = output, 1 = input
+    // NO _TRISA5
+    // NO _TRISA6
+    _TRISA7 = 0; // 0 = output, 1 = input
+    _TRISA8 = 0; // 0 = output, 1 = input
+    _TRISA9 = 0; // 0 = output, 1 = input
+    _TRISA10 = 0; // 0 = output, 1 = input
+
+
+    // *** Open Drain Configuration ***
+
+    //ODCA = 0x00;         // Set entire PORT
+
+    _ODA0 = 0; // 0 = normal, 1 = open drain
+    _ODA1 = 0; // 0 = normal, 1 = open drain
+    _ODA2 = 0; // 0 = normal, 1 = open drain        
+    _ODA3 = 0; // 0 = normal, 1 = open drain        
+    _ODA4 = 0; // 0 = normal, 1 = open drain 
+    //no ODA5       
+    //no ODA6 
+    _ODA7 = 0; // 0 = normal, 1 = open drain 
+    _ODA8 = 0; // 0 = normal, 1 = open drain 
+    _ODA9 = 0; // 0 = normal, 1 = open drain
+    _ODA10 = 0; // 0 = normal, 1 = open drain
+
+
+    // *** Initial Port settings, entire of individual ***
+
+    //LATTA = 0x8000;   // initial port settings
+
+    _LATA0 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATA1 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATA2 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATA3 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATA4 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    // no _LATA5
+    // no _LATA6
+    _LATA7 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATA8 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATA9 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATA10 = 0; // 0 = low(0v), 1 = high(+3.3V)
+
+    // ****************************
+    // *** Redefine PORT MACROS ***
+    // ****************************
+
+    // *** OUTPUTS ***
+    //#define RESET_LED _LATA0
+
+
+    //*** INPUTS ***
+    //#define ENCDR_SW PORTAbits.RA14
+
+    //***********************
+    //***********************
+    // PORTB initialization *
+    //***********************
+    //***********************
+
+    //*** INPUT / OUTPUT Configuration***
+
+    //TRISB = 0x00;      // Set entire PORT
+
+    // Note: Must be set for analog inputs for A/D.
+
+    _TRISB0 = 0; // 0 = output, 1 = input 
+    _TRISB1 = 1; // 0 = output, 1 = input
+    _TRISB2 = 1; // 0 = output, 1 = input
+    _TRISB3 = 1; // 0 = output, 1 = input 
+    _TRISB4 = 1; // 0 = output, 1 = input
+    _TRISB5 = 1; // 0 = output, 1 = input
+    _TRISB6 = 0; // 0 = output, 1 = input
+    _TRISB7 = 0; // 0 = output, 1 = input
+    _TRISB8 = 0; // 0 = output, 1 = input
+    _TRISA9 = 0; // 0 = output, 1 = input
+    _TRISA10 = 0; // 0 = output, 1 = input
+    _TRISB11 = 0; // 0 = output, 1 = input
+    _TRISB12 = 0; // 0 = output, 1 = input
+    _TRISB13 = 0; // 0 = output, 1 = input
+    _TRISB14 = 0; // 0 = output, 1 = input
+    _TRISB15 = 0; // 0 = output, 1 = input
+
+    // *** Open Drain Configuration ***
+
+    //ODCB = 0x00;         // Set entire PORT
+
+    _ODB0 = 0; // 0 = normal, 1 = open drain
+    _ODB1 = 0; // 0 = normal, 1 = open drain
+    _ODB2 = 0; // 0 = normal, 1 = open drain        
+    _ODB3 = 0; // 0 = normal, 1 = open drain        
+    _ODB4 = 0; // 0 = normal, 1 = open drain 
+    _ODB5 = 0; // 0 = normal, 1 = open drain         
+    _ODB6 = 0; // 0 = normal, 1 = open drain
+    _ODB7 = 0; // 0 = normal, 1 = open drain  
+    _ODB8 = 0; // 0 = normal, 1 = open drain 
+    _ODB9 = 0; // 0 = normal, 1 = open drain 
+    _ODB10 = 0; // 0 = normal, 1 = open drain 
+    _ODB11 = 0; // 0 = normal, 1 = open drain 
+    _ODB12 = 0; // 0 = normal, 1 = open drain 
+    _ODB13 = 0; // 0 = normal, 1 = open drain 
+    _ODB14 = 0; // 0 = normal, 1 = open drain 
+    _ODB15 = 0; // 0 = normal, 1 = open drain 
+
+    // *** Initial Port settings, entire of individual ***
+
+    LATB = 0x00; // Set entire PORT
+
+    //_LATB0 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB1 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB2 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB3 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB4 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB5 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB6 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB7 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB8 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB9 = 0;        // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB10 = 0;       // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB11 = 0;       // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB12 = 0;       // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB13 = 0;       // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB14 = 0;       // 0 = low(0v), 1 = high(+3.3V)
+    //_LATB15 = 0;       // 0 = low(0v), 1 = high(+3.3V)
+
+    //************************
+    //************************
+    //* PortC initialization *
+    //************************
+    //************************
+
+
+    //*** INPUT / OUTPUT Configuration***
+
+    //TRISC = 0x00;         // Set entire PORT
+
+    _TRISC0 = 0; // 0 = output, 1 = input
+    _TRISC1 = 0; // 0 = output, 1 = input
+    _TRISC2 = 0; // 0 = output, 1 = input
+    _TRISC3 = 0; // 0 = output, 1 = input 
+    _TRISC4 = 0; // 0 = output, 1 = input
+    _TRISC5 = 0; // 0 = output, 1 = input
+    _TRISC6 = 0; // 0 = output, 1 = input
+    _TRISC7 = 0; // 0 = output, 1 = input 
+    _TRISC8 = 0; // 0 = output, 1 = input
+    _TRISC9 = 0; // 0 = output, 1 = input
+    // no _TRISC10
+    // no _TRISC11
+    // no _TRISC12
+    // no _TRISC13
+    // no _TRISC14
+    // no _TRISC15
+
+    // *** Open Drain Configuration ***
+
+    //ODCC = 0x00;         // Set entire PORT
+
+    _ODC0 = 0; // 0 = normal, 1 = open drain
+    _ODC1 = 0; // 0 = normal, 1 = open drain
+    _ODC2 = 0; // 0 = normal, 1 = open drain        
+    _ODC3 = 0; // 0 = normal, 1 = open drain        
+    _ODC4 = 0; // 0 = normal, 1 = open drain 
+    _ODC5 = 0; // 0 = normal, 1 = open drain
+    _ODC6 = 0; // 0 = normal, 1 = open drain
+    _ODC7 = 0; // 0 = normal, 1 = open drain        
+    _ODC8 = 0; // 0 = normal, 1 = open drain        
+    _ODC9 = 0; // 0 = normal, 1 = open drain 
+    // no _ODC10
+    // no _ODC11
+    // no _ODC12
+    // no _ODC13
+    // no _ODC14
+    // no _ODC15
+
+    // *** Initial Port settings, entire of individual ***
+
+    //LATTC = 0x6a;   // initial port settings
+
+    _LATC0 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC1 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC2 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC3 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC4 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC5 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC6 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC7 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC8 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    _LATC9 = 0; // 0 = low(0v), 1 = high(+3.3V)
+    // no _RC10
+    // no _RC11
+    // no _RC12
+    // no _RC13
+    // no _RC14
+    // no _RC15
+}
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
