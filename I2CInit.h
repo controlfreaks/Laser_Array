3fede779c5a3b04b2d57246c5cec87cd8985058d
James Vlasblom <jamesvlasblom@gmail.com>
2018-06-21 12:08:08 -0700

    I2CRead_Temp finished, not fully tested.

--- I2CInit.h
+++ I2CInit.h
@@ -50,10 +50,64 @@ void I2CInit(void) {
 }
 
 int I2CRead_Temp(void) {
-    int temp = 0; // temperature variable returned.
+    I2C2RCV = 0;    // *** Clear the register for testing
 
+    I2C2CONbits.SEN = 1; // Start bit.
+    while (I2C2CONbits.SEN) { // Wait for Start bit to clear.
+    }
 
-    return temp;
+    I2C2TRN = TEMPSENSE_WRITE; // Write to slaved address with WRITE request.
+
+    // while (I2C2STATbits.TBF) { // Waiting for transmit to finish
+    // } // before testing the ACKSTAT bit.
+
+    //DelayUs(5); // Necessary delay for ACKSTAT to settle.
+
+    while (I2C2STATbits.ACKSTAT) { // Waiting for !ACK from slave.
+        LineWrite_XY_ILI9341_16x25("ACK1 = 1", 0, Line4, ILI9341_WHITE, ILI9341_BLACK); // ***test line ***
+    }
+
+    LineWrite_XY_ILI9341_16x25("ACK1 = 0", 0, Line4, ILI9341_WHITE, ILI9341_BLACK); // ***test line ***
+
+    I2C2TRN = RTR; // write command register
+
+    // while (I2C2STATbits.TBF) { // Waiting for transmit to finish
+    // } // before testing the ACKSTAT bit.
+
+    //DelayUs(5); // Necessary delay for ACKSTAT to settle.
+
+    while (I2C2STATbits.ACKSTAT) { // Waiting for !ACK from slave.
+        LineWrite_XY_ILI9341_16x25("ACK2 = 1", 0, Line5, ILI9341_WHITE, ILI9341_BLACK); // ***test line ***
+    }
+
+    LineWrite_XY_ILI9341_16x25("ACK2 = 0", 0, Line5, ILI9341_WHITE, ILI9341_BLACK); // ***test line ***
+
+    I2C2CONbits.RSEN = 1; // Sending a restart to change direction of bus.
+    while (I2C2CONbits.RSEN) { // Wait for Start bit to clear.
+    }
+    
+    I2C2TRN = TEMPSENSE_READ; // Write to slaved address with READ request
+
+    // while (I2C2STATbits.TBF) { // Waiting for transmit to finish
+    // } // before testing the ACKSTAT bit.
+
+    //DelayUs(5); // Necessary delay for ACKSTAT to settle.
+
+    while (I2C2STATbits.ACKSTAT) { // Waiting for !ACK from slave.
+        LineWrite_XY_ILI9341_16x25("ACK3 = 1", 0, Line6, ILI9341_WHITE, ILI9341_BLACK); // ***test line ***
+    }
+
+    LineWrite_XY_ILI9341_16x25("ACK3 = 0", 0, Line6, ILI9341_WHITE, ILI9341_BLACK); // ***test line ***
+
+    I2C2CONbits.RCEN = 1; // Receiving DATA from of bus.
+    while (I2C2CONbits.RCEN) { // Wait for Start bit to clear.
+    }
+
+    I2C2CONbits.PEN = 1; // Generate stop bit.
+    while (I2C2CONbits.PEN) { // Wait for Start bit to clear.
+    }
+
+    return I2C2RCV; // return the contents of the RCV Register.
 }
 
 void I2CWrite_Config(int command) {
@@ -61,10 +115,10 @@ void I2CWrite_Config(int command) {
     while (I2C2CONbits.SEN) { // Wait for Start bit to clear.
     }
 
-    I2C2TRN = TEMPSENSE_WRITE; // Write to slaved address.
+    I2C2TRN = TEMPSENSE_WRITE; // Write to slaved address with WRITE request.
 
-   // while (I2C2STATbits.TBF) { // Waiting for transmit to finish
-   // } // before testing the ACKSTAT bit.
+    // while (I2C2STATbits.TBF) { // Waiting for transmit to finish
+    // } // before testing the ACKSTAT bit.
 
     //DelayUs(5); // Necessary delay for ACKSTAT to settle.
 
@@ -76,8 +130,8 @@ void I2CWrite_Config(int command) {
 
     I2C2TRN = RWCR; // write command register
 
-   // while (I2C2STATbits.TBF) { // Waiting for transmit to finish
-   // } // before testing the ACKSTAT bit.
+    // while (I2C2STATbits.TBF) { // Waiting for transmit to finish
+    // } // before testing the ACKSTAT bit.
 
     //DelayUs(5); // Necessary delay for ACKSTAT to settle.
 
@@ -86,7 +140,7 @@ void I2CWrite_Config(int command) {
     }
 
     LineWrite_XY_ILI9341_16x25("ACK2 = 0", 0, Line5, ILI9341_WHITE, ILI9341_BLACK); // ***test line ***
-    
+
     I2C2TRN = command; // One of two DATA commands, NORMAL or STANDBY
 
     //while (I2C2STATbits.TBF) { // Waiting for transmit to finish
@@ -99,9 +153,9 @@ void I2CWrite_Config(int command) {
     }
 
     LineWrite_XY_ILI9341_16x25("ACK3 = 0", 0, Line6, ILI9341_WHITE, ILI9341_BLACK); // ***test line ***
-    
+
     I2C2CONbits.PEN = 1; // Generate stop bit.
-        while (I2C2CONbits.PEN) { // Wait for Start bit to clear.
+    while (I2C2CONbits.PEN) { // Wait for Start bit to clear.
     }
 }
 
